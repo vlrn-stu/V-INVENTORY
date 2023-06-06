db08b3d55c087cafcd23aa1ac297f6370e98885b
v <v@pongda@gmail.com>
2023-06-06 14:16:56 -0700

    ### Changed - Move UploadedImage to Shared.Models

---
+++ V-INVENTORY.WEB/Shared/Models/UploadedImage.cs
@@ -0,0 +1,9 @@
+﻿namespace V_INVENTORY.WEB.Shared.Models
+{
+    public class UploadedImage
+    {
+        public string? Name { get; set; }
+        public string? ImageData { get; set; }
+        public byte[]? ByteArray { get; set; }
+    }
+}
